2efed1027cbf03b0bdce56cbdb01a7a618fbe0b0
zanjs <root@zanjs.com>
2017-06-20 18:21:19 +0800

    If you are doing your best,you will not have to worry about failure. :raised_hands:

--- polls/models.py
+++ polls/models.py
@@ -1,16 +1,31 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length = 200)
+    """
+    Question is
+    """
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        """
+        was_published_recently is
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
+    """
+    Choice is
+    """
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
